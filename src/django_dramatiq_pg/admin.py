1bd56f384241058ab81405fd960a04b5b5fcf754
Curtis Maloney <curtis@tinbrain.net>
2020-03-02 12:08:08 +1100

    Show actor in Admin

--- src/django_dramatiq_pg/admin.py
+++ src/django_dramatiq_pg/admin.py
@@ -5,5 +5,8 @@ from .models import Actor
 
 @admin.register(Actor)
 class ActorAdmin(admin.ModelAdmin):
-    list_display = ('message_id', 'queue_name', 'state',)
+    list_display = ('message_id', 'queue_name', 'actor', 'state',)
     list_filter = ('queue_name', 'state',)
+
+    def actor(self, obj):
+        return obj.message['actor_name']
