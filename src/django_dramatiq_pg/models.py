1c3b9e0458acd01c41d86d1f74504b96cf35b4d6
Curtis Maloney <curtis@tinbrain.net>
2020-06-15 11:02:41 +1000

    Refine field definitions. Bind state enum to model.

--- src/django_dramatiq_pg/models.py
+++ src/django_dramatiq_pg/models.py
@@ -2,9 +2,10 @@ from enum import Enum
 
 from django.contrib.postgres.fields import JSONField
 from django.db import models
+from django.utils import timezone
 
 
-class State(Enum):
+class State(str, Enum):
     QUEUED = "queued"
     CONSUMED = "consumed"
     REJECTED = "rejected"
@@ -17,11 +18,13 @@ State.choices = tuple((state.value, state.name) for state in State)
 class BackgroundJob(models.Model):
     message_id = models.UUIDField(primary_key=True)
     queue_name = models.TextField(default="default")
-    state = models.TextField(default=State.QUEUED.value, choices=State.choices)
-    mtime = models.DateTimeField()  # Default = now()
-    message = JSONField()
-    result = JSONField()
+    state = models.CharField(max_length=16, default=State.QUEUED, choices=State.choices)
+    mtime = models.DateTimeField(default=timezone.now)
+    message = JSONField(blank=True, null=True)
+    result = JSONField(blank=True, null=True)
     result_ttl = models.DateTimeField()
+
+    STATUS = State
 
     class Meta:
         managed = False
