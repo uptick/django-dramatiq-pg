643575d193efe66d30035d55f143d65445e6d7cb
Marc Fister <mwfister@gmail.com>
2020-07-29 13:19:38 +1000

    Change BGJob to use Django's TextChoices

---
+++ CHANGES.rst
@@ -0,0 +1,10 @@
+=========
+CHANGELOG
+=========
+
+
+1.2.0 (development)
+===================
+
++ BackgroundJob now uses Django's TextChoices instead of a custom enum
+- Removed support for Django <=2.2
--- src/django_dramatiq_pg/models.py
+++ src/django_dramatiq_pg/models.py
@@ -1,30 +1,23 @@
-from enum import Enum
-
 from django.contrib.postgres.fields import JSONField
 from django.db import models
 from django.utils import timezone
 
 
-class State(str, Enum):
-    QUEUED = "queued"
-    CONSUMED = "consumed"
-    REJECTED = "rejected"
-    DONE = "done"
-
-
-State.choices = tuple((state.value, state.name) for state in State)
-
-
 class BackgroundJob(models.Model):
+
+    class STATUS(models.TextChoices):
+        QUEUED = "queued"
+        CONSUMED = "consumed"
+        REJECTED = "rejected"
+        DONE = "done"
+
     message_id = models.UUIDField(primary_key=True)
     queue_name = models.TextField(default="default")
-    state = models.CharField(max_length=16, default=State.QUEUED, choices=State.choices)
+    state = models.CharField(max_length=16, default=STATUS.QUEUED, choices=STATUS.choices)
     mtime = models.DateTimeField(default=timezone.now)
     message = JSONField(blank=True, null=True)
     result = JSONField(blank=True, null=True)
     result_ttl = models.DateTimeField()
-
-    STATUS = State
 
     class Meta:
         managed = False
